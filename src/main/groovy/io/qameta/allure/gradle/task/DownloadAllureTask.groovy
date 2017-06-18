41eabd4e16fc7ad9e4171083572710440bb43383
Artem Eroshenko <eroshenkoam@qameta.io>
2017-06-18 14:49:40 +0300

    add optional parameter

--- src/main/groovy/io/qameta/allure/gradle/task/DownloadAllureTask.groovy
+++ src/main/groovy/io/qameta/allure/gradle/task/DownloadAllureTask.groovy
@@ -3,6 +3,7 @@ package io.qameta.allure.gradle.task
 import io.qameta.allure.gradle.AllureExtension
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 
@@ -14,6 +15,7 @@ class DownloadAllureTask extends DefaultTask {
     static final String NAME = 'downloadAllure'
 
     @Input
+    @Optional
     String version
 
     @Input

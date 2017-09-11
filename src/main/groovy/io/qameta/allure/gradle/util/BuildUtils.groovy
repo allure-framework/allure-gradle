48cc15891190685bba264e42a1fb5c57c066ab4f
Artem Eroshenko <eroshenkoam@qameta.io>
2017-09-11 16:57:59 +0300

    copy categories from resources (fixes #21, via #23)

--- src/main/groovy/io/qameta/allure/gradle/util/BuildUtils.groovy
+++ src/main/groovy/io/qameta/allure/gradle/util/BuildUtils.groovy
@@ -1,6 +1,8 @@
 package io.qameta.allure.gradle.util
 
 import groovy.json.JsonOutput
+import org.gradle.api.Project
+import org.gradle.api.tasks.SourceSetContainer
 
 import java.nio.file.Paths
 
@@ -8,6 +10,10 @@ import java.nio.file.Paths
  * @author Egor Borisov ehborisov@gmail.com
  */
 class BuildUtils {
+
+    public static final String EXECUTOR_FILE_NAME = 'executor.json'
+
+    public static final String CATEGORIES_FILE_NAME = 'categories.json'
 
     static boolean isWindows() {
         return System.getProperty('os.name').toLowerCase().contains('win')
@@ -17,9 +23,21 @@ class BuildUtils {
         return isWindows() ? 'allure.bat' : 'allure'
     }
 
-    static void copyExecutorInfo(File resultsDir, Map<String, String> executorInfo) {
-        File executorInfoFile = Paths.get(resultsDir.absoluteFile.path).resolve('executor.json').toFile()
-        executorInfoFile.text = JsonOutput.toJson(executorInfo)
+    static void copyExecutorInfo(File resultsDir, Project project) {
+        Map<String, String> executorInfo = [name: 'Gradle', type: 'gradle', buildName: project.displayName]
+        File executorFile = Paths.get(resultsDir.absoluteFile.path).resolve(EXECUTOR_FILE_NAME).toFile()
+        executorFile.text = JsonOutput.toJson(executorInfo)
+    }
+
+    static void copyCategoriesInfo(File resultsDir, Project project) {
+        SourceSetContainer sourceSets = (SourceSetContainer) project.properties.get('sourceSets')
+        List<File> resourcesCategoriesFiles = sourceSets.getByName('test').resources
+                .findAll { file -> (file.name == CATEGORIES_FILE_NAME) }
+
+        if (!resourcesCategoriesFiles.isEmpty()) {
+            File categoriesFile = Paths.get(resultsDir.absoluteFile.path).resolve(CATEGORIES_FILE_NAME).toFile()
+            categoriesFile.text = resourcesCategoriesFiles.first().text
+        }
     }
 
 }

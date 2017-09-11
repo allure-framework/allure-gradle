b4caa27d18c018b57c60a5f913cde36049865721
Artem Eroshenko <eroshenkoam@qameta.io>
2017-09-11 16:26:59 +0300

    codestyle fixes

--- src/main/groovy/io/qameta/allure/gradle/util/BuildUtils.groovy
+++ src/main/groovy/io/qameta/allure/gradle/util/BuildUtils.groovy
@@ -11,6 +11,10 @@ import java.nio.file.Paths
  */
 class BuildUtils {
 
+    public static final String EXECUTOR_FILE_NAME = 'executor.json'
+
+    public static final String CATEGORIES_FILE_NAME = 'categories.json'
+
     static boolean isWindows() {
         return System.getProperty('os.name').toLowerCase().contains('win')
     }
@@ -21,17 +25,17 @@ class BuildUtils {
 
     static void copyExecutorInfo(File resultsDir, Project project) {
         Map<String, String> executorInfo = [name: 'Gradle', type: 'gradle', buildName: project.displayName]
-        File executorFile = Paths.get(resultsDir.absoluteFile.path).resolve('executor.json').toFile()
+        File executorFile = Paths.get(resultsDir.absoluteFile.path).resolve(EXECUTOR_FILE_NAME).toFile()
         executorFile.text = JsonOutput.toJson(executorInfo)
     }
 
     static void copyCategoriesInfo(File resultsDir, Project project) {
-        SourceSetContainer sourceSets = (SourceSetContainer) project.getProperties().get('sourceSets')
-        List<File> resourcesCategoriesFiles = sourceSets.getByName('test').getResources()
-                .findAll { file -> (file.name == 'categories.json') }
+        SourceSetContainer sourceSets = (SourceSetContainer) project.properties.get('sourceSets')
+        List<File> resourcesCategoriesFiles = sourceSets.getByName('test').resources
+                .findAll { file -> (file.name == CATEGORIES_FILE_NAME) }
 
         if (!resourcesCategoriesFiles.isEmpty()) {
-            File categoriesFile = Paths.get(resultsDir.absoluteFile.path).resolve("categories.json").toFile()
+            File categoriesFile = Paths.get(resultsDir.absoluteFile.path).resolve(CATEGORIES_FILE_NAME).toFile()
             categoriesFile.text = resourcesCategoriesFiles.first().text
         }
     }

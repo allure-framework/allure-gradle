628d27f6ee39dad7e0be4734a23dce12f6dd42f2
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2021-04-19 22:31:25 +0300

    Bump Gradle to 6.8.3, FindBugs -> SpotBugs

--- src/main/groovy/io/qameta/allure/gradle/task/AllureReport.groovy
+++ src/main/groovy/io/qameta/allure/gradle/task/AllureReport.groovy
@@ -5,13 +5,13 @@ import io.qameta.allure.gradle.AllureReportContainer
 import io.qameta.allure.gradle.util.BuildUtils
 import org.gradle.api.Action
 import org.gradle.api.DefaultTask
-import org.gradle.api.internal.ClosureBackedAction
 import org.gradle.api.reporting.Reporting
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import org.gradle.util.ClosureBackedAction
 
 import java.nio.file.Files
 import java.nio.file.Path

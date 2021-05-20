cb07f416f57eafc5cdb5c2dfe16bb7dfcff82bdc
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2021-05-20 12:16:00 +0300

    Skip signing at JitPack build

--- build-logic/publishing/src/main/kotlin/allure-gradle.signing.gradle.kts
+++ build-logic/publishing/src/main/kotlin/allure-gradle.signing.gradle.kts
@@ -6,8 +6,7 @@ plugins.withId("maven-publish") {
 // so activate signing for release versions only
 // Just in case Maven Central rejects signed snapshots for some reason
 plugins.withId("signing") {
-    if (!version.toString().endsWith("-SNAPSHOT")) {
-        apply(plugin = "signing")
+    if (!version.toString().endsWith("-SNAPSHOT") && System.getenv("JITPACK")?.toBoolean() != true) {
         configure<SigningExtension> {
             sign(the<PublishingExtension>().publications)
         }

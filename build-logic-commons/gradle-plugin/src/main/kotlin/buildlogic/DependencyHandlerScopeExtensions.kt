4c7e16c93a34a64659f9765a1cd195ec7c9c3b4c
Alexey Zhokhov <alexey@zhokhov.com>
2021-09-27 23:10:05 +0800

    Build fix after upgrade to Gradle 7.2.

--- build-logic-commons/gradle-plugin/src/main/kotlin/buildlogic/DependencyHandlerScopeExtensions.kt
+++ build-logic-commons/gradle-plugin/src/main/kotlin/buildlogic/DependencyHandlerScopeExtensions.kt
@@ -1,17 +1,30 @@
 package buildlogic
 
+import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.DependencyHandlerScope
 import org.gradle.kotlin.dsl.`kotlin-dsl`
 import org.gradle.plugin.use.PluginDependenciesSpec
+import org.gradle.plugin.use.PluginDependency
 import org.gradle.plugin.use.PluginDependencySpec
 
 val DependencyHandlerScope.kotlinDslVersion: String
-    get() = PluginDependenciesSpec {
-        object : PluginDependencySpec {
-            var version: String? = null
-            override fun version(version: String?) = apply { this.version = version }
-            override fun apply(apply: Boolean) = this
-            override fun toString() = version ?: ""
+    get() = object : PluginDependenciesSpec {
+        override fun id(id: String): PluginDependencySpec {
+            return object : PluginDependencySpec {
+                var version: String? = null
+                override fun version(version: String?) = apply { this.version = version }
+                override fun apply(apply: Boolean) = this
+                override fun toString() = version ?: ""
+            }
+        }
+
+        override fun alias(notation: Provider<PluginDependency>): PluginDependencySpec {
+            return object : PluginDependencySpec {
+                var version: String? = null
+                override fun version(version: String?) = apply { this.version = version }
+                override fun apply(apply: Boolean) = this
+                override fun toString() = version ?: ""
+            }
         }
     }.`kotlin-dsl`.toString()
 

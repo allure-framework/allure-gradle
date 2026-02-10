8fdb4a9caf4328fcea70bb14e4ec63f9ffd70449
Ivan Ponomarev <iponomarev@mail.ru>
2026-02-10 15:26:31 +0000

    Fix: Make `allure-results` a cacheable output directory (fixes #107, via #154)

---
+++ allure-adapter-plugin/src/it/adapter-cache-junit5-kts/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("java")
+    id("io.qameta.allure-adapter")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ allure-adapter-plugin/src/it/adapter-cache-junit5-kts/src/test/java/tests/SimpleTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+    @Test
+    void passes() {
+        assertTrue(true);
+    }
+}
